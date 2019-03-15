862186017f7151a1cd5b328cf087e3bd1a5e7b22
Phuc Pham <phuc.pham@sympliact.com>
2019-03-15 18:13:20 -0500

    A simple socketio application. Lets see if deploying will work

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,45 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, send, emit, disconnect
+
+async_mode = "eventlet"
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+
 
 @app.route('/')
 def index():
-    return 'Sign Language Glove!'
+    return render_template('index.html')
 
-# @app.route('/form')
-# def form():
-#     return render_template('form.html')
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my_response', {'data': message['data']})
 
-# @app.route('/submitted', methods=['POST'])
-# def submitted_form():
-#     name = request.form['name']
-#     email = request.form['email']
-#     site = request.form['site_url']
-#     comments = request.form['comments']
-#     return render_template(
-#         'submitted_form.html',
-#         name=name,
-#         email=email,
-#         site=site,
-#         comments=comments)
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my_response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my_response', {'data': 'You is Connected'})
+
+@socketio.on('my_ping', namespace='/test')
+def ping_pong():
+    emit('my_pong')
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
