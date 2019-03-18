9192513cd8e1ef4683c1b65699942a84e4ee4720
Phuc Pham <phuc.pham@sympliact.com>
2019-03-17 21:22:37 -0500

    Cleaned up some code

--- backend/main.py
+++ backend/main.py
@@ -12,30 +12,30 @@ socketio = SocketIO(app, async_mode=async_mode)
 def index():
     return render_template('index.html')
 
-@socketio.on('my event', namespace='/test')
+@socketio.on('my event')
 def test_message(message):
     emit('my_response', {'data': message['data']})
 
-@socketio.on('my broadcast event', namespace='/test')
+@socketio.on('my broadcast event')
 def test_message(message):
     emit('my_response', {'data': message['data']}, broadcast=True)
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect')
 def test_connect():
-    emit('my_response', {'data': 'You is Connected'})
+    emit('my_response', {'data': 'Connected!'})
 
-@socketio.on('my_ping', namespace='/test')
+@socketio.on('my_ping')
 def ping_pong():
     emit('my_pong')
 
-@socketio.on('disconnect_request', namespace='/test')
+@socketio.on('disconnect_request')
 def disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': 'Disconnected!', 'count': session['receive_count']})
     disconnect()
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
 
