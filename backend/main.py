cb2e5949d4515fc2a5921b9c19a5b9d0c9a471a2
Phuc Pham <phuc.pham@sympliact.com>
2019-04-04 00:25:29 -0500

    Successfully loaded the model and got it to return predictions for a given set of readings

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, send, emit, disconnect
+from predict import predict
 
 async_mode = "eventlet"
 
@@ -13,14 +14,22 @@ def index():
     return render_template('index.html')
 
 @socketio.on('echo')
-def test_message(message):
+def echo(message):
     print('echo: ', message)
     emit('my_response', message)
 
 @socketio.on('broadcasting')
-def test_message(message):
+def broadcast(message):
     print('broadcasting: ', message)
     emit('my_response', message, broadcast=True)
+
+@socketio.on('msg')
+def msg(message):
+    if (message == ''):
+        message = '-5488 11796 5320 4151 3606 -8208 535 777 768 745 867'
+    print('msg: ', message)
+    result = predict(message)
+    emit('prediction', result)
 
 @socketio.on('connect')
 def test_connect():
@@ -34,8 +43,7 @@ def ping_pong():
 @socketio.on('disconnect_request')
 def disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
+    emit('my_response', 'Disconnected!')
     disconnect()
 
 @socketio.on('disconnect')
---
+++ backend/predict.py
@@ -0,0 +1,55 @@
+from sklearn.externals import joblib
+import numpy as np
+import sys
+
+letterMap = {
+	0: "a",
+	1: "b",
+	2: "c",
+	3: "d",
+	4: "e",
+	5: "f",
+	6: "g",
+	7: "h",
+	8: "i",
+	9: "j",
+}
+
+
+def predict(data):
+	filename = sys.argv[-1]
+
+	loaded_model = joblib.load("model.pkl")
+	# x_data = []
+	# with open(filename) as fp:
+	# 	for cnt, line in enumerate(fp):
+	# 		x_data.append(line.split()[1:-1])
+	# X_test_final = []
+
+
+	X_test = np.zeros(11)
+	X_test[0] = 0
+	X_test[1] = 11796
+	X_test[2] = 5320
+	X_test[3] = 4151
+	X_test[4] = 3606
+	X_test[5] = -8208
+	X_test[6] = 535
+	X_test[7] = 777
+	X_test[8] = 768
+	X_test[9] = 745
+	X_test[10] = 867
+
+	data = data.split(' ')
+
+	# X_test_final.append(X_test)
+	Y_predicted = loaded_model.predict([data])
+	# for pred, sample in zip(Y_predicted, X_test_final):
+	# 	print(letterMap[pred])
+	# 	result = letterMap[pred]
+	print('Y_predicted: ', Y_predicted)
+	print(letterMap[Y_predicted[0]])
+	return letterMap[Y_predicted[0]]
+		
+
+	# 5488 11796 5320 4151 3606 -8208 535 777 768 745 867 
